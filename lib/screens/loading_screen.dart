5464bc14c1d5b511b03607a62733057e68d23ef6
heberton-cabral <hebertonayrton@hotmail.com>
2023-10-04 17:09:06 -0300

    aula 08

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:tempo_template/services/location_service.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -8,6 +12,33 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  Future<void> getLocation() async {
+    var locationService = LocationService();
+    var location = await locationService.getCurrentLocation();
+    log("Localização atual: lat: ${location.latitude}, long: ${location.longitude}");
+  }
+
+  Future<void> getWeatherData() async {
+    var url = Uri.parse(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      var data = response.body;
+      log(data);
+    } else {
+      log(response.statusCode.toString());
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+    getWeatherData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +46,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
         child: ElevatedButton(
           onPressed: () {
             // obtém a localização atual
+            getLocation();
           },
           child: const Text('Obter Localização'),
         ),
---
+++ lib/services/location_service.dart
@@ -0,0 +1,41 @@
+import 'package:geolocator/geolocator.dart';
+import 'package:tempo_template/models/location.dart';
+
+class LocationService {
+  Future<Location> getCurrentLocation() async {
+    await checkLocationPermission();
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.low,
+    );
+
+    var location = Location(
+      latitude: position.latitude,
+      longitude: position.longitude,
+    );
+
+    return location;
+  }
+
+  Future<void> checkLocationPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // serviço de localização desabilitado. Não será possível continuar
+      return Future.error('O serviço de localização está desabilitado.');
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Sem permissão para acessar a localização
+        return Future.error('Sem permissão para acesso à localização');
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      // permissões negadas para sempre
+      return Future.error(
+          'A permissão para acesso a localização foi negada para sempre. Não é possível pedir permissão.');
+    }
+  }
+}
